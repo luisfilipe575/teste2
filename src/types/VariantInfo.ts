00183dbdeaf6af0db30885e6f0414aee4f2047d9
Ryan Haticus <ryan@haticus.com>
2021-08-03 18:48:38 -0500

    TSDocs for VariantInfo type

--- src/types/VariantInfo.ts
+++ src/types/VariantInfo.ts
@@ -1,7 +1,19 @@
 import Product from './Product';
 import Variant from './Variant';
 
+/**
+ * @remarks
+ * The VariantInfo type used by the Printful API.
+ */
 export default interface VariantInfo {
+  /**
+   * @remarks
+   * Information about the selected Variant
+   */
   variant: Variant;
+  /**
+   * @remarks
+   * Information about the Product that the Variant belongs to
+   */
   product: Product;
 }
