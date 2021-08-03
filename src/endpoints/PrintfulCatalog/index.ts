2d6278e538096bb565daa88f1b8f06e9da238dd8
Ryan Haticus <ryan@haticus.com>
2021-08-03 00:04:38 -0500

    TSDoc comments for the products function.

--- src/endpoints/PrintfulCatalog/index.ts
+++ src/endpoints/PrintfulCatalog/index.ts
@@ -7,6 +7,14 @@ export default class PrintfulCatalog {
   constructor(requester: Requester) {
     this.requester = requester;
   }
+  /**
+   * @returns
+   * Returns list of Products available in the Printful.
+   *
+   * @example
+   * const products = await printful.PrintfulCatalog.products();
+   * console.log(products[0]); // Log the first product!
+   */
   async products(): Promise<ProductsResponse | ErrorResponse> {
     return await this.requester.request('products', RequesterMethod.GET, null);
   }
