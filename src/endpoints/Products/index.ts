e327e5a3b09acba6b6d94e3fd8c09e685913a462
Ryan Haticus <ryan@haticus.com>
2021-08-04 23:47:49 -0500

    Change to how optional inputs are handled!

--- src/endpoints/Products/index.ts
+++ src/endpoints/Products/index.ts
@@ -3,7 +3,10 @@ import { ErrorResponse } from '../../types/ErrorResponse';
 import RequestVariant from '../../types/RequestVariant';
 import SyncProduct from '../../types/SyncProduct';
 import createProduct, { CreateProductResponse } from './createProduct';
-import listProducts, { ListProductsResponse } from './listProducts';
+import listProducts, {
+  ListProductsParams,
+  ListProductsResponse
+} from './listProducts';
 
 export default class Products {
   private requester!: Requester;
@@ -14,25 +17,24 @@ export default class Products {
    * @remarks
    * Get a list of sync products.
    *
-   * @param status - Filter by item status (`synced/unsynced/all`). If only some of the variants are synced,the product is returned by both unsynced and synced filters
-   * @param search - Product search needle
-   * @param offset - Result set offset
-   * @param limit - Number of items per page (max 100)
+   * @param params - The input parameters
    *
    * @example
-   * const { result } = await printful.Products.listProducts('synced');
+   * const { result } = await printful.Products.listProducts({ status: 'synced' });
    * console.log(result[0]); // Log the first synced 'backpack' product!
    *
    * @see
    * https://www.printful.com/docs/products#listProducts
    */
   async listProducts(
-    status?: string,
-    search?: string,
-    offset?: number,
-    limit?: number
+    params: ListProductsParams
   ): Promise<ListProductsResponse | ErrorResponse> {
-    return await listProducts(this.requester, status, search, offset, limit);
+    return await listProducts(this.requester, {
+      status: params.status,
+      search: params.search,
+      limit: params.limit,
+      offset: params.offset
+    });
   }
   /**
    * @remarks
--- src/endpoints/Products/listProducts.ts
+++ src/endpoints/Products/listProducts.ts
@@ -8,21 +8,34 @@ export interface ListProductsResponse extends RequesterResponse {
   paging: Paging;
 }
 
+/**
+ * @remarks
+ * Because this function has multiple optional input parameters, we have organized them into an object for you.
+ *
+ * @param status - Filter by item status (`synced/unsynced/all`). If only some of the variants are synced,the product is returned by both unsynced and synced filters
+ * @param search - Product search needle
+ * @param offset - Result set offset
+ * @param limit - Number of items per page (max 100)
+ */
+export interface ListProductsParams {
+  status?: string;
+  search?: string;
+  offset?: number;
+  limit?: number;
+}
+
 const listProducts = async (
   requester: Requester,
-  status?: string,
-  search?: string,
-  offset?: number,
-  limit?: number
+  params: ListProductsParams
 ): Promise<ListProductsResponse | ErrorResponse> => {
   return await requester.request<ListProductsResponse>(
     `store/products`,
     RequesterMethod.GET,
     {
-      status,
-      search,
-      offset,
-      limit
+      status: params.status,
+      search: params.search,
+      offset: params.offset,
+      limit: params.limit
     }
   );
 };
