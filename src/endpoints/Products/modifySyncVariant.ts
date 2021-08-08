5097132cacccda59cefbdbce4577f76c1c128547
Ryan Haticus <ryan@haticus.com>
2021-08-08 14:38:52 -0500

    One last thing!

--- src/endpoints/Products/modifySyncVariant.ts
+++ src/endpoints/Products/modifySyncVariant.ts
@@ -1,9 +1,10 @@
 import Requester, { RequesterMethod, RequesterResponse } from '../../requester';
 import { ErrorResponse } from '../../types/ErrorResponse';
 import PutRequestVariant from '../../types/PutRequestVariant';
+import RequestVariantResponse from '../../types/RequestVariantResponse';
 
 export interface ModifySyncVariantResponse extends RequesterResponse {
-  result: PutRequestVariant;
+  result: RequestVariantResponse;
 }
 
 const modifySyncVariant = async (
